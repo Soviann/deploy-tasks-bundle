d278b8649c89787b8cc208b770196e6e36f0cd1d
nicolasvasse <nicolas.vasse@siqual.fr>
2026-04-13 18:17:01 +0200

    test: adds events and lock DI wiring functional tests

---
+++ tests/Functional/LockEnabledTestKernel.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Soviann\DeployTasks\Tests\Functional;
+
+use Soviann\DeployTasks\Tests\Fixtures\SimpleTask;
+use Soviann\DeployTasksBundle\DeployTasksBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Kernel;
+
+final class LockEnabledTestKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles(): iterable
+    {
+        yield new FrameworkBundle();
+        yield new DeployTasksBundle();
+    }
+
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
+        $container->extension('framework', [
+            'test' => true,
+            'secret' => 'test',
+            'http_method_override' => false,
+            'handle_all_throwables' => true,
+            'php_errors' => ['log' => true],
+            'lock' => true, // registers lock.factory service
+        ]);
+
+        $storagePath = \sys_get_temp_dir().'/deploy-tasks-lock-'.$this->environment;
+
+        $container->extension('deploy_tasks', [
+            'storage' => [
+                'type' => 'filesystem',
+                'filesystem' => ['path' => $storagePath],
+            ],
+            'events' => ['enabled' => false],
+            'lock' => ['enabled' => true],
+        ]);
+
+        $container->services()
+            ->set('test.task.simple', SimpleTask::class)
+            ->args(['test.simple', 'A simple test task'])
+            ->tag('deploy_tasks.task')
+        ;
+    }
+
+    public function getCacheDir(): string
+    {
+        return \sys_get_temp_dir().'/deploy-tasks-lock-cache/'.$this->environment;
+    }
+
+    public function getLogDir(): string
+    {
+        return \sys_get_temp_dir().'/deploy-tasks-lock-logs';
+    }
+}
